84b6b88c7823279edb1183037998d5e9326676d4
YutakaX17 <kademptr@gmail.com>
2026-08-03 23:11:22 +0200

    feat: publish Android messages UI variant

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.jetbrains.compose) apply false
+    alias(libs.plugins.android.kmp.library) apply false
 }
 
 group = "io.github.yutakax17.advancedhelloworld"
--- messages-ui/build.gradle.kts
+++ messages-ui/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.android.kmp.library)
     `maven-publish`
 }
 
@@ -11,6 +12,12 @@ version = rootProject.version
 kotlin {
     explicitApi()
     jvm()
+    android {
+        namespace = "io.github.yutakax17.advancedhelloworld.compose.messages"
+        compileSdk = 37
+        minSdk = 24
+        androidResources.enable = true
+    }
 
     sourceSets {
         commonMain.dependencies {
